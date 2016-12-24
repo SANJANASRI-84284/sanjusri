1fee3f57679545989a53335def159773a9595941
SANJANASRI-84284 <sanjusri962936@gmail.com>
2016-12-24 12:32:35 +0530

    Add files via upload

---
+++ 13.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+    char ch;
+printf("\nenter the character");
+scanf("%c",&ch);
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='u'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+printf("\nthe character is vowel");
+else
+printf("\nthe character is consonant");\
+getch();
+}
