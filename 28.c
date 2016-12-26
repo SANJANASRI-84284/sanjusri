13e5137678fdf044eed44417b655222ffc4e1c3a
SANJANASRI-84284 <sanjusri962936@gmail.com>
2016-12-26 10:13:30 +0530

    Add files via upload

---
+++ 28.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int j,n,s=0;
+printf("\n enter the number:");
+scanf("%d",&n);
+for(j=0;j<=n;j++)
+{
+s+=i;
+}
+printf("\ns=%d",s);
+getch();
+}
